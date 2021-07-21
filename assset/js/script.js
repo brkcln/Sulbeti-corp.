46a8c0fe080f0bbfb36d3ba6b9c5f30c46edbd31
Burak <burakcelen17@gmail.com>
2021-07-21 12:31:22 -0700

    Add files via upload

--- assset/js/script.js
+++ assset/js/script.js
@@ -1,48 +1,72 @@
-var tl = anime.timeline({
-	easing: "easeOutExpo",
-	duration: 1000,
-	delay: 1000,
+anime({
+	targets: ".buble",
+	translateX: "-10",
+	duration: 1700,
+	opacity: [0, 1],
+	delay: 750,
+	easing: "easeOutQuint",
+	loop: false, // increase delay by 100ms for each elements.
+});
+anime({
+	targets: ".buble2",
+	translateX: "10",
+	duration: 1700,
+	delay: 1750,
+	opacity: [0, 1],
+	easing: "easeOutQuint",
+	loop: false, // increase delay by 100ms for each elements.
+});
+anime({
+	targets: ".buble3",
+	translateX: "-10",
+	duration: 1700,
+	delay: 2750,
+	opacity: [0, 1],
+	easing: "easeOutQuint",
+	loop: false, // increase delay by 100ms for each elements.
+});
+anime({
+	targets: ".bubble-speaker",
+	translateX: 5,
+	duration: 2000,
 	loop: false,
+	opacity: [0, 1],
+	easing: "easeOutQuint",
+	// increase delay by 100ms for each elements.
+});
+anime({
+	targets: ".feature-second",
+	translateX: "-5",
+	duration: 1800,
+	opacity: [0, 1],
+	delay: 1750,
+	easing: "easeOutQuint",
+	loop: false, // increase delay by 100ms for each elements.
+});
+anime({
+	targets: ".feature-first",
+	translateX: "-5",
+	duration: 1800,
+	opacity: [0, 1],
+	delay: 750,
+	easing: "easeOutQuint",
+	loop: false, // increase delay by 100ms for each elements.
+});
+anime({
+	targets: ".feature-last",
+	translateX: "-5",
+	duration: 1800,
+	opacity: [0, 1],
+	delay: 2750,
+	easing: "easeOutQuint",
+	loop: false, // increase delay by 100ms for each elements.
 });
 
-// Add children
-tl.add({
-	targets: ".a",
-	translateX: 20,
-	opacity: 1,
-	delay: 1000,
-})
-	.add({
-		targets: ".b",
-		translateX: -20,
-		opacity: 1,
-		delay: 0,
-	})
-	.add({
-		targets: ".c",
-		translateX: 20,
-		opacity: 1,
-		delay: 0,
-	})
-	.add({
-		targets: ".d",
-		translateX: -20,
-		opacity: 1,
-		delay: 0,
-	})
-	.add({
-		targets: ".e",
-		translateX: 20,
-		opacity: 1,
-		delay: 0,
-	});
-anime({
-	targets: ".hero-img",
-	translateY: 20,
-	translateX: 20,
-	left: "33.5%",
-	top: "-5%",
-	easing: "easeOutCubic",
-	opacity: 1,
-	loop: false,
-});
+function myFunction() {
+	let x = document.getElementById("nav-flex");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
